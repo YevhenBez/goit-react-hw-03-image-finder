004b46daa3eba31b9dafc28a0983079681cd3bb0
YevhenBez <byxgalter@bigmir.net>
2023-05-14 22:22:37 +0300

    Переделал Арр на компонент и внёс немного разметки и корректировок для REACT  Задание 2 Модуль 3   REACT
    Переделал Арр на компонент и внёс немного разметки и корректировок для REACT    А именно - в файле   App.jsx   импортнул    React   и компонент    Searchbar     Арр переделал на компонент и добавил внутри него     render() { }      и разметку компонента    Searchbar     и, как резюме, экспортнул    Арр
     В файле   Searchbar.js   внёс корректировки, чтобы команды мог прочитать    REACT   и также экспортнул компонент   Searchbar   А в файле   index.js   сделал импорт   REACT-компонента  Арр. Задание 2 Модуль 3   REACT

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React from 'react';
+import Searchbar from './searchbar/Searchbar';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+        {/* React homework template */}
+        <Searchbar  />
+      </div>
+    );
+  }
 };
+
+export default App;
