494e0605bb1e9cd0ba40bb8de5a03225ac684737
YevhenBez <byxgalter@bigmir.net>
2023-05-14 16:31:50 +0300

    Создал компонент    Searchbar   Задал ему значение Стэйт и вернул разметку согласно ТЗ. Задание 2 Модуля 3   REACT
    Создал компонент    Searchbar   в Стэйте которого буду хранить значение введённое пользователем в поле Поиска. Также вернул разметку согласно ТЗ. Задание 2 Модуля 3   REACT

---
+++ src/components/searchbar/Searchbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Searchbar extends React.Component {
+  state = {
+    inputData: '',
+  };
+
+  render() {
+    return (
+      <header class="searchbar">
+        <form class="form">
+          <button type="submit" class="button">
+            <span class="button-label">Search</span>
+          </button>
+
+          <input
+            class="input"
+            type="text"
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
