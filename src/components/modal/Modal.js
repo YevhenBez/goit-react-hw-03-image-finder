3092c5235d1c78bd90beb0e0cc2e2f7f4fcdaf5b
YevhenBez <byxgalter@bigmir.net>
2023-05-15 14:55:26 +0300

    Сделал закрытие модального окна при помощи кнопки   Escape     Задание 2 Модуль 3   REACT
    Сделал закрытие модального окна при помощи кнопки   Escape  Для этого создал Колбэк   keyDown   , который срабатывает если это была кнопка  Escape   изменяя при этом признак открытия Модального окна. А также, для полной картины - используя методы жизненного цикла повесил слушателя на   window  (чтобы он определял момент нажатия кнопки  Escape)     А при помощи метода жизненного цикла  componentWillUnmount     снял слушателя, чтобы не было зацикливания при нажатии на клавишу  Escape  Задание 2 Модуль 3   REACT

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -4,6 +4,19 @@ import { createPortal } from 'react-dom';
 const modalRoot = document.querySelector('#modalRoot');
 
 class Modal extends React.Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.keyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.keyDown);
+  }
+
+  keyDown = event => {
+    if (event.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
   render() {
     return createPortal(
       <div className="overlay">
