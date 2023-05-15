d94c63386ec0cdc435fa97023ad717d74968d29b
YevhenBez <byxgalter@bigmir.net>
2023-05-16 02:33:46 +0300

    Прописал импорты необходимых компонентов и создал Светофор-машину, которая в зависимости от состояния возвращает ту или иную разметку.  Задание 2 Модуля 3   REACT
    Прописал импорты необходимых компонентов и создал Светофор-машину, которая в зависимости от состояния возвращает ту или иную разметку. Для этого задал начальное значение страницы при помощи   let page = 1;     В Стэйте задал начальное значение поиска (то что будет вводить пользователь)    inputData       , пустой массив для базы фоток     items      , начальный статус и начальное значение общего количества фоток   totalHits    . Далее функция   handleSubmit    пошла раздавать статусы в зависимости от значения, которое пользователь ввёл в поле Поиска, а также, при соответствующем условии, перезаписывать Стэйты.    Функция    onNextPage необходима для кнопки ЗАГРУЗИТЬ БОЛЬШЕ   При удачной загрузке она дозапишет очередную порцию фоток в Стэйт   items       изменив значение    Статуса на    resolved    или поймает ошибку (изменив статус на соответствующий    rejected     Далее расписывается возврат соответствующей разметки в зависимости от статуса прописанного в Стэйт функциями указанными выше.   Задание 2 Модуля 3   REACT

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,132 @@
 import React from 'react';
 import Searchbar from './searchbar/Searchbar';
+import Button from './button/Button';
+import ImageGallery from './ImageGallery/ImageGallery';
+import { fetchImages } from './fetchImages/fetchImages';
+import Loader from './loader/Loader';
+
+let page = 1;
 
 class App extends React.Component {
+  state = {
+    inputData: '',
+    items: [],
+
+    status: 'idle',
+    totalHits: 0,
+  };
+
+  handleSubmit = async inputData => {
+    page = 1;
+    if (inputData.trim() === '') {
+      return;
+    } else {
+      try {
+        this.setState({ status: 'pending' });
+        const { totalHits, hits } = await fetchImages(inputData, page);
+        if (hits.length < 1) {
+          this.setState({ status: 'idle' });
+        } else {
+          this.setState({
+            items: hits,
+            inputData,
+            totalHits: totalHits,
+            status: 'resolved',
+          });
+        }
+      } catch (error) {
+        this.setState({ status: 'rejected' });
+      }
+    }
+  };
+  onNextPage = async () => {
+    this.setState({ status: 'pending' });
+
+    try {
+      const { hits } = await fetchImages(this.state.inputData, (page += 1));
+      this.setState(prevState => ({
+        items: [...prevState.items, ...hits],
+        status: 'resolved',
+      }));
+    } catch (error) {
+      this.setState({ status: 'rejected' });
+    }
+  };
   render() {
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101'
-        }}
-      >
-        {/* React homework template */}
-        <Searchbar  />
-      </div>
-    );
+    const { totalHits, status, items } = this.state;
+    if (status === 'idle') {
+      return (
+        <div
+          style={{
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: 40,
+            color: '#010101',
+          }}
+        >
+          <Searchbar onSubmit={this.handleSubmit} />
+        </div>
+      );
+    }
+    if (status === 'pending') {
+      return (
+        <div
+          style={{
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: 40,
+            color: '#010101',
+          }}
+        >
+          <Searchbar onSubmit={this.handleSubmit} />
+          <ImageGallery page={page} items={this.state.items} />
+          <Loader />
+          {totalHits > 12 && <Button onClick={this.onNextPage} />}
+        </div>
+      );
+    }
+    if (status === 'rejected') {
+      return (
+        <div
+          style={{
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: 40,
+            color: '#010101',
+          }}
+        >
+          <Searchbar onSubmit={this.handleSubmit} />
+          <p>Something wrong, try later</p>
+        </div>
+      );
+    }
+    if (status === 'resolved') {
+      return (
+        <div
+          style={{
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: 40,
+            color: '#010101',
+          }}
+        >
+          <Searchbar onSubmit={this.handleSubmit} />
+          <ImageGallery page={page} items={this.state.items} />
+          {totalHits > 12 && totalHits > items.length && (
+            <Button onClick={this.onNextPage} />
+          )}
+        </div>
+      );
+    }
   }
-};
+}
 
 export default App;
