8ce61eeb15d8bb8d8bb3cb6aca224d0ecae13362
YevhenBez <byxgalter@bigmir.net>
2023-05-15 13:16:49 +0300

    Использовал метод Реакта    createPortal    и вернул разметку модального Окна согласно ТЗ. Задание 2 Модуль 3   REACT
    Использовал метод Реакта    createPortal    и вернул разметку модального Окна согласно ТЗ.  Для этого импортнул метод Реакта    createPortal   при помощи   import { createPortal } from 'react-dom';     и разрендерил разметку модалки согласно ТЗ при помощи данного метода  использовав команду    return createPortal( )   Внутри которой помимо разметки Модалки согласно ТЗ, я вставил разметку, которая позволяет быть Модалке сверху всех окон, а именно   modalRoot   Задание 2 Модуль 3   REACT

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { createPortal } from 'react-dom';
 
 const modalRoot = document.querySelector('#modalRoot');
 
 class Modal extends React.Component {
-    
+  render() {
+    return createPortal(
+      <div className="overlay">
+        <div className="modal">
+          <img src="" alt="" />
+        </div>
+      </div>,
+      modalRoot
+    );
+  }
 }
