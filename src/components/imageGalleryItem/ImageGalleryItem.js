2733d50c4ed528ba06a99b3533046a162b8f2c53
YevhenBez <byxgalter@bigmir.net>
2023-05-15 14:21:55 +0300

    Создал функцию изменения признака открытия Модального окна и передал её Пропсом в компонент Модального окна. Задание 2 Модуля 3   REACT
    Создал функцию  onModal   для  изменения признака открытия Модального окна и передал её Пропсом в компонент Модального окна при помощи  onClose={this.onModal} . Задание 2 Модуля 3   REACT

--- src/components/imageGalleryItem/ImageGalleryItem.js
+++ src/components/imageGalleryItem/ImageGalleryItem.js
@@ -5,11 +5,14 @@ class ImageGalleryItem extends React.Component {
   state = {
     shownModal: false,
   };
+  onModal = () => {
+    this.setState(({ shownModal }) => ({ shownModal: !shownModal }));
+  };
   render() {
     return (
       <li class="gallery-item">
-            <img src="" alt="" />
-            {this.state.shownModal && <Modal  />}
+        <img src="" alt="" />
+        {this.state.shownModal && <Modal onClose={this.onModal}/>}
       </li>
     );
   }
